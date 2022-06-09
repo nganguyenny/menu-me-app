b1a5f52ae5bc50c59e7ceca1a5ccf0875a787006
Nga Nguyen <nganguyen.ny@gmail.com>
2022-06-09 16:53:05 +0800

    add message after menu is built

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ if uploaded_file is not None:
     bucket = client.get_bucket('menu_me_bucket')
     blob = bucket.blob("img.jpg")
     blob.upload_from_filename("img.jpg")
-    st.write('Photo is uploaded 🥳')
+    st.success('Photo is uploaded 🥳')
 
     with st.spinner('Your menu is coming soon... 🌮 🌯 🥙'):
         # TEST WITH SEED DATABASE
@@ -59,5 +59,5 @@ if uploaded_file is not None:
         # url = '/dish'
         # response = requests.get(url).json()
 
-    st.write('Enjoy your meals! 🥰')
+    st.success('Enjoy your meals! 🥰')
 
