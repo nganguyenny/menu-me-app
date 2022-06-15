47c92f9348dec1048ed686e841b9caf04deabe59
Nga Nguyen <nganguyen.ny@gmail.com>
2022-06-16 04:22:51 +0800

    test config streamlit/config.toml

--- setup.sh
+++ setup.sh
@@ -2,16 +2,14 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \'nganguyen.ngocyen@gmail.com\'\n\
+email = \"nganguyen.ngocyen@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
-enableXsrfProtection = false\n\
-enableWebsocketCompression = false\n\
 port = $PORT\n\
-[theme]\n\
+[theme]
 base = 'light'\n\
 " > ~/.streamlit/config.toml
