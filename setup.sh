99a83d306713c4ed921bd5433a8ccbfc3725166f
Nga Nguyen <nganguyen.ny@gmail.com>
2022-06-16 04:17:29 +0800

    test config streamlit/config.toml

--- setup.sh
+++ setup.sh
@@ -2,17 +2,18 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"nganguyen.ngocyen@gmail.com\"\n\
+email = \'nganguyen.ngocyen@gmail.com\'\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
+enableXsrfProtection = false\n\
+enableWebsocketCompression = false\n\
 port = $PORT\n\
-[theme]
+[theme]\n\
 base = 'light'\n\
-enableXsrfProtection = false\n
-enableWebsocketCompression = false\n
-serverAddress = 'https://menu-me-app.herokuapp.com/'\n
+[browser]\n\
+serverAddress = 'https://menu-me-app.herokuapp.com/'\n\
 " > ~/.streamlit/config.toml
