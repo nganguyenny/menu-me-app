59e903f2d7b51c448b5270158169bab926b2ac35
Nga Nguyen <nganguyen.ny@gmail.com>
2022-06-16 04:08:46 +0800

    test echo streamlit/config.toml

--- setup.sh
+++ setup.sh
@@ -5,11 +5,13 @@ echo "\
 email = \"nganguyen.ngocyen@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-[theme]
-base = 'light'\n\
-" > ~/.streamlit/config.toml
+# echo "\
+# [server]\n\
+# headless = true\n\
+# enableCORS=false\n\
+# port = $PORT\n\
+# [theme]
+# base = 'light'\n\
+# " > ~/.streamlit/config.toml
+
+echo .streamlit/config.toml > ~/.streamlit/config.toml
